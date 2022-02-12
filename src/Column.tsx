7598a5c4445f5a882197458ad8f702e953d2eb75
MZ1387 <maza1387@gmail.com>
2022-02-11 22:04:48 -0800

    render AddnNewItem component in Column Container component

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,5 +1,6 @@
 import { ColumnContainer, ColumnTitle } from "./styles";
 import { Card } from './Card';
+import { AddNewItem } from './AddNewItem';
 
 // define the form of your props object
 type ColumnProps = {
@@ -13,6 +14,11 @@ export const Column = ({ text }: ColumnProps) => {
             <Card text='task one' />
             <Card text='task two' />
             <Card text='task three' />
+            <AddNewItem
+                toggleButtonText="+ Add another card"
+                onAdd={console.log}
+                dark
+            />
         </ColumnContainer>
     );
 };
