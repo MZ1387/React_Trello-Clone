c9e659ce3a1e33069dea18ca9926d628b3a13b6e
MZ1387 <maza1387@gmail.com>
2022-02-11 20:53:49 -0800

    render all styled components together

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import {
+  AppContainer,
+  ColumnContainer,
+  ColumnTitle,
+  CardContainer
+} from './styles';
 
 export const App = () => {
-  return null
+  return (
+    <AppContainer>
+      <ColumnContainer>
+        <ColumnTitle>Todo: </ColumnTitle>
+        <CardContainer>First Item</CardContainer>
+        <CardContainer>Second Item</CardContainer>
+        <CardContainer>Third Item</CardContainer>
+      </ColumnContainer>
+    </AppContainer>
+  )
 };
