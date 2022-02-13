a0ad82aef83926e80e8cce7a644391ce1cb14803
MZ1387 <maza1387@gmail.com>
2022-02-13 13:24:51 -0800

    add card component to CustomDragLayer component

--- src/CustomDragLayer.tsx
+++ src/CustomDragLayer.tsx
@@ -2,6 +2,7 @@ import { useDragLayer } from "react-dnd";
 import { Column } from "./Column";
 import { CustomDragLayerContainer, DragPreviewWrapper } from "./styles";
 import { useAppState } from "./state/AppStateContext";
+import { Card } from "./Card";
 
 export const CustomDragLayer = () => {
     const { draggedItem } = useAppState();
@@ -12,11 +13,20 @@ export const CustomDragLayer = () => {
     return draggedItem && currentOffset ? (
         <CustomDragLayerContainer>
             <DragPreviewWrapper position={currentOffset} >
-                <Column 
-                    id={draggedItem.id}
-                    text={draggedItem.text}
-                    isPreview
-                />
+                { draggedItem.type === 'COLUMN' ? (
+                    <Column 
+                        id={draggedItem.id}
+                        text={draggedItem.text}
+                        isPreview
+                    />
+                ) : (
+                    <Card 
+                        id={draggedItem.id}
+                        columnId={draggedItem.columnId}
+                        text={draggedItem.text}
+                        isPreview
+                    />
+                )}
             </DragPreviewWrapper>
         </CustomDragLayerContainer>
     ) : null;
