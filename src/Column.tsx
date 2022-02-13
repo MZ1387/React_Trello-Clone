7d886fc196c8d795aaacfe937d541bff28628203
MZ1387 <maza1387@gmail.com>
2022-02-13 13:50:57 -0800

    drag card to an empty column

--- src/Column.tsx
+++ src/Column.tsx
@@ -6,7 +6,7 @@ import { useRef } from 'react';
 import { useItemDrag } from './utils/useItemDrag';
 import { useDrop } from "react-dnd";
 import { throttle } from 'throttle-debounce-ts';
-import { moveList, addTask } from './state/actions';
+import { moveList, addTask, moveTask, setDraggedItem } from './state/actions';
 import { isHidden } from './utils/isHidden';
 
 // define the form of your props object
@@ -22,7 +22,7 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
     const ref = useRef<HTMLDivElement>(null);
 
     const [, drop] = useDrop({
-        accept: 'COLUMN',
+        accept: ['COLUMN', 'CARD'],
         hover: throttle(200, () => {
 
             if (!draggedItem) {
@@ -36,6 +36,18 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
                 }
 
                 dispatch(moveList(draggedItem.id, id));
+            } else {
+
+                if (draggedItem.columnId === id) {
+                    return;
+                }
+
+                if (tasks.length) {
+                    return;
+                }
+
+                dispatch(moveTask(draggedItem.id, null, draggedItem.columnId, id));
+                dispatch(setDraggedItem({ ...draggedItem, columnId: id }));
             }
         })
     });
